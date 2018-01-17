626d88bbce13f861b32b6fba67d9bc57e54bd248
graySquirrel <fritzebner@gmail.com>
2018-01-17 15:37:05 -0500

    updated files done with demo

--- compile.js
+++ compile.js
@@ -5,8 +5,10 @@ const solc = require('solc');
 const inboxPath = path.resolve(__dirname,'contracts','Inbox.sol');
 const source = fs.readFileSync(inboxPath, 'utf8');
 
+// all the npm install things had to do at command line powershell
 // npm install --save solc
 // npm install --save mocha ganache-cli web3@1.0.0-beta.26
+// npm install --save truffle-hdwallet-provider
 
 //console.log(solc.compile(source, 1));
 // can see if powershell: node ./compile.js
--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -4,4 +4,41 @@ const Web3 = require('web3'); //caps because its a constructor
 
 // make an instance
 // ganache is the network to connect the instance to
-const web3 = new Web3(ganache.provider());
+const provider = ganache.provider();
+const web3 = new Web3(provider);
+
+// get the compiled contract interface (ABI) and bytecode
+const { interface, bytecode } = require('../compile');
+
+const INITIAL_STRING = 'Hi fucker!';
+const NEW_MESSAGE = 'A New One';
+
+let accounts;
+let inbox;
+beforeEach(async () => {
+  // Get a list of all accounts
+  accounts = await web3.eth.getAccounts();
+  // Use one of the accounts to deploy the contract
+  inbox = await new web3.eth.Contract(JSON.parse(interface))
+    .deploy({ data: bytecode, arguments: [INITIAL_STRING] })
+    .send({ from: accounts[0], gas: '1000000' });
+  inbox.setProvider(provider);
+});
+
+describe('Inbox', () => {
+  it('deploys a contract', () => {
+    assert.ok(inbox.options.address);
+    console.log(inbox.options.address);
+  });
+
+  it('has the correct default message', async () => {
+    const message = await inbox.methods.message().call();
+    assert.equal(message, INITIAL_STRING)
+  });
+
+  it('can update message', async () => {
+    await inbox.methods.setMessage(NEW_MESSAGE).send({ from: accounts[0]});
+    const message = await inbox.methods.message().call();
+    assert.equal(message, NEW_MESSAGE)
+  });
+})
